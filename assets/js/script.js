9e19e2e17a4a89dc12c35eaadaecc192830c2b8f
Noah Stiltner <nstilt1@lsu.edu>
2023-05-21 15:39:55 -0500

    fixed some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
         var hash = this.hash;
   
         // Subtract desired offset. In this case, we're assuming a header height of 60px.
-        var scrollPoint = $(hash).offset().top - 105;
+        var scrollPoint = $(hash).offset().top - 145;
   
         $('html, body').animate({
           scrollTop: scrollPoint
-        }, 800, function(){
+        }, 420, function(){
           window.location.hash = hash - 60; // This will not change the actual ID of the element.
         });
       }
